68f6fc8ae5daa11c6eb5fb0be24b8060d570a2e3
Sergey Ilinov <ilinovsg@gmail.com>
2020-07-09 16:15:43 +0700

    Реализована поддержка аргументов запуска терминального приложения.

---
+++ src/ru/ilinovsg/tm/constant/TerminalConst.java
@@ -0,0 +1,9 @@
+package ru.ilinovsg.tm.constant;
+
+public class TerminalConst {
+
+    public static final String HELP = "help";
+    public static final String ABOUT = "about";
+    public static final String VERSION = "version";
+
+}
